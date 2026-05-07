0e5bc5b8fe657323556fffac7040c8bd17a7c642
matheuslazari <mthslazari@gmail.com>
2026-05-07 16:29:21 -0300

    Criado a controller aula 2

---
+++ src/main/java/dev/matheus/CadastroDeNinjas/Controller.java
@@ -0,0 +1,18 @@
+package dev.matheus.CadastroDeNinjas;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping
+
+
+public class Controller {
+
+    @GetMapping("/boasvindas")
+    public String boasVindas() {
+        return "Essa é minha primeira mensagem nessa rota";
+    }
+
+}
