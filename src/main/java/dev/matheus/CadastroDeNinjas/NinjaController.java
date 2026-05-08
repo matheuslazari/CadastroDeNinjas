5eda79ec93b1e0007b3dde99990ab89f4b363164
matheuslazari <mthslazari@gmail.com>
2026-05-08 16:12:51 -0300

    Creates an entity in the database from ninjaModel [Aula3]

--- src/main/java/dev/matheus/CadastroDeNinjas/NinjaController.java
+++ src/main/java/dev/matheus/CadastroDeNinjas/NinjaController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping
 
 
-public class Controller {
+public class NinjaController {
 
     @GetMapping("/boasvindas")
     public String boasVindas() {
