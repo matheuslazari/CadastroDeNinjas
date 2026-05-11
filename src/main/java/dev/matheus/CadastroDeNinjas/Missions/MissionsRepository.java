ff15cd4e3e988e8a6ba3d37acccf58e56dafda57
matheuslazari <mthslazari@gmail.com>
2026-05-11 17:30:37 -0300

    Feat: Add NinjaService, NinjaRepository and MissionRepository

---
+++ src/main/java/dev/matheus/CadastroDeNinjas/Missions/MissionsRepository.java
@@ -0,0 +1,5 @@
+package dev.matheus.CadastroDeNinjas.Missions;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface MissionsRepository extends JpaRepository<MissionsModel, Long> {
+}
---
+++ src/main/java/dev/matheus/CadastroDeNinjas/Ninjas/NinjaRepository.java
@@ -0,0 +1,5 @@
+package dev.matheus.CadastroDeNinjas.Ninjas;
+
+public class NinjaRepository
+{
+}
