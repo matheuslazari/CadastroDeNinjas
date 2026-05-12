c4fe2f21a746e8987da6a07f0dab2ebb8c6d4fbe
matheuslazari <mthslazari@gmail.com>
2026-05-12 15:34:35 -0300

    fix: Adds @Collum into class

--- src/main/java/dev/matheus/CadastroDeNinjas/Ninjas/NinjaModel.java
+++ src/main/java/dev/matheus/CadastroDeNinjas/Ninjas/NinjaModel.java
@@ -15,13 +15,19 @@ public class NinjaModel {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column (name = "id")
     private Long id;
 
+    @Column (name = "name")
     private String name;
 
     @Column(unique = true)
     private String email;
 
+    @Column (name = "img_url")
+    private String imgUrl;
+
+    @Column (name = "age")
     private int age;
 
     // @ManyToOne a ninja have only one mission
