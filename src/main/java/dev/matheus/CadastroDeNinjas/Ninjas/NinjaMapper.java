79df9c1a9d8826dab6b4eb86f18bc892f2cd8801
matheuslazari <mthslazari@gmail.com>
2026-05-18 15:51:49 -0300

    chore: includes DTO version by entity and map manually into NinjaMapper entityxdto way

--- src/main/java/dev/matheus/CadastroDeNinjas/Ninjas/NinjaMapper.java
+++ src/main/java/dev/matheus/CadastroDeNinjas/Ninjas/NinjaMapper.java
@@ -1,4 +1,35 @@
 package dev.matheus.CadastroDeNinjas.Ninjas;
+import org.springframework.stereotype.Component;
 
+@Component
 public class NinjaMapper {
+
+    public NinjaModel map(NinjaDTO ninjaDTO) {
+        NinjaModel ninjaModel = new NinjaModel();
+        ninjaModel.setId(ninjaDTO.getId());
+        ninjaModel.setName(ninjaDTO.getName());
+        ninjaModel.setEmail(ninjaDTO.getEmail());
+        ninjaModel.setAge(ninjaDTO.getAge());
+        ninjaModel.setImgUrl(ninjaDTO.getImgUrl());
+        ninjaModel.setRank(ninjaDTO.getRank());
+        ninjaModel.setMissions(ninjaDTO.getMissions());
+
+        return ninjaModel;
+    }
+
+    public NinjaDTO map(NinjaModel ninjaModel) {
+
+        NinjaDTO ninjaDTO = new NinjaDTO();
+        ninjaDTO.setId(ninjaModel.getId());
+        ninjaDTO.setName(ninjaModel.getName());
+        ninjaDTO.setEmail(ninjaModel.getEmail());
+        ninjaDTO.setAge(ninjaModel.getAge());
+        ninjaDTO.setImgUrl(ninjaModel.getImgUrl());
+        ninjaDTO.setRank(ninjaModel.getRank());
+        ninjaDTO.setMissions(ninjaModel.getMissions());
+
+        return ninjaDTO;
+    }
+
+
 }
